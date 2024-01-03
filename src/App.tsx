3c80ec574f08716b5ece030f4b9cddae9c684500
Rybnikov.VV <va6d6@mail.ru>
2024-01-04 03:15:56 +0500

    lesson 1_7

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
-import { Outlet, Route, Routes, Link } from 'react-router-dom';
-import "./app.scss";
-import About from './pages/AboutPage/About';
-import Main from './pages/MainPage/Main';
+import { Route, Routes, Link } from 'react-router-dom';
+import "./styles/index.scss";
 import MainLazy from './pages/MainPage/MainLazy';
 import AboutLazy from './pages/AboutPage/AboutLazy';
 import { Suspense } from 'react';
+import useTheme from './theme/useTheme';
 
 const App = () => {
+  const { theme, changeTheme } = useTheme();
+
   return (
-    <div className='app'>
+    <div className={`app ${theme}`}>
+      <button onClick={changeTheme}>toggleTheme</button>
       <Link to={'/'}>Главная</Link>
       <Link to={'/about'}>О нас</Link>
       <Suspense fallback={<div>Loading...</div>}>
